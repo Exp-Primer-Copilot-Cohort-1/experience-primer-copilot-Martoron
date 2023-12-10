382a2d39ba82fa72d190a6a5fcfdbb07d74095ce
Martoron <109730337+Martoron@users.noreply.github.com>
2023-12-10 09:58:51 +0000

    fixes name

--- member.js
+++ member.js
@@ -1,12 +1,14 @@
-function skillmembers() {
-    var skill = document.getElementById("skill").value;
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-
-            document.getElementById("skillmembers").innerHTML = this.responseText;
-        }
-    };
-    xhttp.open("GET", "skillmembers.php?skill=" + skill, true);
-    xhttp.send();
+function skillsmembers() {
+    var skills = document.getElementById("skills");
+    var members = document.getElementById("members");
+    var skillsmembers = document.getElementById("skillsmembers");
+    var skillsbutton = document.getElementById("skillsbutton");
+    var membersbutton = document.getElementById("membersbutton");
+    var skillsmembersbutton = document.getElementById("skillsmembersbutton");
+    skills.style.display = "none";
+    members.style.display = "none";
+    skillsmembers.style.display = "block";
+    skillsbutton.style.color = "white";
+    membersbutton.style.color = "white";
+    skillsmembersbutton.style.color = "black";
 }
